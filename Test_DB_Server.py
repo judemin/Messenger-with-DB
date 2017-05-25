faae180ff35c34a55fb48884bfded53719bbdcb3
judemin <judemin2087@naver.com>
2017-05-25 19:34:20 +0900

    Second Git test

--- Test_DB_Server.py
+++ Test_DB_Server.py
@@ -1,5 +1,7 @@
 import pymysql
 
+print("Please Change DB id and pw")
+
 conn = pymysql.connect(host='localhost', user='root', password='apmsetup',db='messenger_db', charset='utf8')
 curs = conn.cursor()
 sql = "select * from login_info"
