ae439c56f5839a68f026ec7361136b06adbec730
judemin <judemin2087@naver.com>
2017-07-12 17:55:11 +0900

    Added Login Function
    Login Function with table login_info

--- Test_DB_Client.py
+++ Test_DB_Client.py
@@ -1,16 +1,43 @@
+'''
+python Test_DB_Client.py
+'''
 import socket
 
 print("Test Messenger Client")
-userName = input("Input User Name  : ")
 HOST = input("Input IP : ")
 PORT = 50007
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 print("Connected to " + HOST)
-s.sendall(userName.encode())
 
 while True :
-	command = input("Input User Name  : ")
+	print("")
+	tmp = input("1:Login 2:Register - ")
+	if tmp is '1':
+		s.sendall(tmp.encode())
+		tmp_id = input("Input ID : ")
+		s.sendall(tmp_id.encode())
+		tmp_pw = input("Input PW : ")
+		s.sendall(tmp_pw.encode())
+		status = (s.recv(1024)).decode('utf-8')
+		if status is '1' :
+			print("Welcome " + tmp_id)
+			break
+		else :
+			print("ID or PW is Incorrect!")
+
+	elif tmp is '2':
+		s.sendall(tmp.encode())
+		tmp_id = input("Input ID : ")
+		s.sendall(tmp_id.encode())
+		tmp_pw = input("Input PW : ")
+		s.sendall(tmp_pw.encode())
+		status = (s.recv(1024)).decode('utf-8')
+		if status is '1' :
+			print("Registration Complete!")
+		else :
+			print("Registration Error!")
+	else:
+		print("Incorrect Command")
 
 s.close()
-print ("Received" +  repr(data))
--- Test_DB_Server.py
+++ Test_DB_Server.py
@@ -1,3 +1,6 @@
+'''
+python Test_DB_Server.py
+'''
 import pymysql
 import socket
 
@@ -6,8 +9,9 @@ print("Connecting to Database")
 ID='root'
 PW = 'apmsetup'
 DB = 'messenger_db'
-conn = pymysql.connect(host='localhost', user=ID, password=PW,db=DB, charset='utf8')
-curs = conn.cursor()
+table = 'login_info'
+connDB = pymysql.connect(host='localhost', user=ID, password=PW,db=DB, charset='utf8')
+curs = connDB.cursor()
 print("Successfully Connected to " +  DB)
 
 print("Server Socket Listening")
@@ -18,14 +22,59 @@ s.bind((HOST, PORT))
 s.listen(1)
 conn, addr = s.accept()
 print ("Connected by " + addr[0])
-data = conn.recv(1024)
-stringdata = data.decode('utf-8')
-print("UserName : " + stringdata)
-conn.close()
 
 while True :
-	sql = input("Input Query : ")
-	curs.execute(sql) 
-	rows = curs.fetchall()
-	print(rows)
+	data = (conn.recv(1024)).decode('utf-8')
+	if data is '1':
+		print("Login request")
+		isLogin = False
+		tmp_id = (conn.recv(1024)).decode('utf-8')
+		print("Received ID : " + tmp_id)
+		tmp_pw = (conn.recv(1024)).decode('utf-8')
+		print("Received PW : " + tmp_pw)
+		curs.execute("SELECT * FROM " + table) 
+		row = curs.fetchone()
+		while row is not None :
+			if (tmp_id == row[0]) and (tmp_pw == row[1]) :
+				isLogin = True
+				break
+			row = curs.fetchone()
+		if isLogin is True : 
+			tmpM = '1'
+			conn.sendall(tmpM.encode())
+			print("Login Complete from " + tmp_id)
+			break
+		else :
+			tmpM = '0'
+			conn.sendall(tmpM.encode())
+			print("Login Error from " + tmp_id)
+
+	if data is '2':
+		print("Registration request")
+		canCreate = True
+		tmp_id = (conn.recv(1024)).decode('utf-8')
+		print("Received ID : " + tmp_id)
+		tmp_pw = (conn.recv(1024)).decode('utf-8')
+		print("Received PW : " + tmp_pw)
+		curs.execute("SELECT * FROM " + table) 
+		row = curs.fetchone()
+		while row is not None :
+			if tmp_id == row[0] :
+				canCreate = False
+				break
+			row = curs.fetchone()
+		if canCreate is True : 
+			print("Inserting Data to DB...")
+			curs.execute("INSERT INTO " + table + " (info_id,info_pw) VALUES (%s,%s)", 
+			  (tmp_id,tmp_pw))
+			connDB.commit()
+			print("Commit Complete!")
+			tmpM = '1'
+			conn.sendall(tmpM.encode())
+			print("Registration Complete from " + tmp_id)
+		else :
+			tmpM = '0'
+			conn.sendall(tmpM.encode())
+			print("Registration Error from " + tmp_id)
+
 conn.close()
